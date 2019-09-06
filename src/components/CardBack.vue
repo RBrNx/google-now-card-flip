23cf7dcbecf962dcb958436dd852c508fe09178b
Conor Watson <conor.watson@soar.tech>
2019-09-06 20:26:15 +0100

    feat: Created CardBack component

---
+++ src/components/CardBack.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cardBack">
+    <span>{{ description }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardBack",
+  props: ["description"]
+};
+</script>
+
+<style lang="scss">
+.cardBack {
+  background-color: gray;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 25px;
+}
+</style>
