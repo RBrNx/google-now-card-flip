7c567e99cc256f15fbbda5a8963fbc6166ff6d76
Conor Watson <conor.watson@soar.tech>
2019-09-06 15:32:57 +0100

    feat: Created CardFront component

---
+++ src/components/CardFront.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card" :style="{ 'background-color': backgroundColor }" @click="$emit('cardClicked')">
+    <span>{{ colorName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardClone",
+  props: ["backgroundColor", "colorName"]
+};
+</script>
+
+<style lang="scss">
+.card {
+  padding-top: 100px;
+  padding-bottom: 100px;
+  cursor: pointer;
+
+  span {
+    color: #fff;
+    font-weight: bold;
+    font-size: 18px;
+  }
+}
+</style>
